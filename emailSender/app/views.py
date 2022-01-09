a51cd480d0b432e04bb8dbe2b70fed5e69c94390
Oier Mentxaka <oiermentxaka@gmail.com>
2022-01-09 13:12:28 +0100

    20220109

--- emailSender/app/views.py
+++ emailSender/app/views.py
@@ -11,8 +11,6 @@ class Form(FormView):
     success_url = '/confirmation/'
 
     def form_valid(self, form):
-        # This method is called when valid form data has been POSTed.
-        # It should return an HttpResponse.
         form.send_email()
         return super().form_valid(form)
 
